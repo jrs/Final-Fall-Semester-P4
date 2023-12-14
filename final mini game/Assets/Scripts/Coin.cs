86572c1ef3b53833b7aab3728dd38ab1739eefd8
Justin Scott <hello@jrs.dev>
2023-12-14 11:59:36 -0800

    working on milestone 3-4

--- final mini game/Assets/Scripts/Coin.cs
+++ final mini game/Assets/Scripts/Coin.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public AudioClip coinSound;
+    private AudioSource _coinAudio;
+
+    void Start()
+    {
+        _coinAudio = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            _coinAudio.PlayOneShot(coinSound, 1f);
             Debug.Log("Player collected the coin.");
             GameObject.Find("Canvas").GetComponent<UIManager>().UpdateCoinCount();
+            
             Destroy(this.gameObject);
         }
     }
