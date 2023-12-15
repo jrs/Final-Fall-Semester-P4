0253489f84680ce8d5564b065876c975d1a3615b
Justin Scott <hello@jrs.dev>
2023-12-15 14:17:50 -0800

    updating code

--- final mini game/Assets/Scripts/Coin.cs
+++ final mini game/Assets/Scripts/Coin.cs
@@ -6,22 +6,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public AudioClip coinSound;
-    private AudioSource _coinAudio;
-
-    void Start()
-    {
-        _coinAudio = GetComponent<AudioSource>();
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            _coinAudio.PlayOneShot(coinSound, 1f);
             Debug.Log("Player collected the coin.");
             GameObject.Find("Canvas").GetComponent<UIManager>().UpdateCoinCount();
-            
             Destroy(this.gameObject);
         }
     }
--- final mini game/Assets/Scripts/PlayerController.cs
+++ final mini game/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
+    public AudioClip coinSound;
 
     private float _horizontalInput;
     private float _forwardInput;
     private Rigidbody _playerRb;
+    private AudioSource _playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
+        _playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,5 +28,14 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDirection = new Vector3(_horizontalInput, 0f, _forwardInput);
         _playerRb.AddForce(moveDirection * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            _playerAudio.PlayOneShot(coinSound, 1f);
+            GameObject.Find("Spawn Manager").GetComponent<SpawnManager>().SpawnCollectibleObject();
+        }
     }
 }
